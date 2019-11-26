0137f0201768a9112824350a069859ba8e2c83fd
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MBP.lan1>
2019-11-26 13:53:35 -0500

    Loading score on sks completed

--- Game_score/Asteroid/viewscore.swift
+++ Game_score/Asteroid/viewscore.swift
@@ -15,12 +15,12 @@ class viewscore: SKScene{
     override func didMove(to view: SKView)
     {
         var list : [model] = []
-        let winner = SKLabelNode(fontNamed: "Chalkduster")
-        winner.text = "Winner's Board"
-        winner.fontSize = 100
-        winner.fontColor = SKColor.green
-        winner.position = CGPoint(x: frame.midX, y: frame.midY)
-
+        let title = SKLabelNode(fontNamed: "Chalkduster")
+        title.text = "Score Board"
+        title.fontSize = 100
+        title.fontColor = SKColor.black
+        title.position = CGPoint(x: frame.midX, y: frame.height/2-100)
+        addChild(title)
          guard let urlpath = URL(string: "https://cs.binghamton.edu/~pmadden/courses/441score/getscores.php") else{
                       return
                   }
@@ -37,11 +37,59 @@ class viewscore: SKScene{
                                         let player = model(dictionary: datalist)
                                         list.append(player)
                                   }
+                                  displaylist();
                                   } catch {
                                       print(error)
                                   }
                               }
                           }.resume()
-                                                print(list)
+        func displaylist(){
+            var i = frame.height/2-300
+            var j = frame.height/2-300
+            var k = frame.height/2-300
+            let heading1 = SKLabelNode(fontNamed: "Chalkduster")
+            heading1.text = "Game"
+            heading1.fontSize = 40
+            heading1.fontColor = SKColor.black
+            heading1.position = CGPoint(x: frame.midX-300, y: i+40)
+            addChild(heading1)
+            let heading2 = SKLabelNode(fontNamed: "Chalkduster")
+            heading2.text = "Player"
+            heading2.fontSize = 40
+            heading2.fontColor = SKColor.black
+            heading2.position = CGPoint(x: frame.midX, y: j+40)
+            addChild(heading2)
+            let heading3 = SKLabelNode(fontNamed: "Chalkduster")
+            heading3.text = "Score"
+            heading3.fontSize = 40
+            heading3.fontColor = SKColor.black
+            heading3.position = CGPoint(x: frame.midX+300, y: k+40)
+            addChild(heading3)
+            for items in list
+            {
+                let temp1 = SKLabelNode(fontNamed: "Chalkduster")
+                let temp2 = SKLabelNode(fontNamed: "Chalkduster")
+                let temp3 = SKLabelNode(fontNamed: "Chalkduster")
+                temp1.text = items.game
+                temp1.fontSize = 30
+                temp1.fontColor = SKColor.black
+                temp1.position = CGPoint(x: frame.midX-300, y: i)
+                temp2.text = items.player
+                temp2.fontSize = 30
+                temp2.fontColor = SKColor.black
+                temp2.position = CGPoint(x: frame.midX, y: j)
+                temp3.text = items.score
+                temp3.fontSize = 30
+                temp3.fontColor = SKColor.black
+                temp3.position = CGPoint(x: frame.midX + 300, y: k)
+                i = i - 40
+                j = j - 40
+                k = k - 40
+                addChild(temp1)
+                addChild(temp2)
+                addChild(temp3)
+            }
+            print(list[1].game)
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Get_score
+# Get_Score
+# Astroid Game
+This IOS application is built on spritkit, this is a space game where we have a player, villian
+The game is about player trying to destroy the enemies
+
+Description:-
+So the one in yellow at bottom is our player and villian's will be coming continously to destroy our player
+
+Player special abilities:-
+1.Bullets that can destroy the enemies
+2.Freeze:- Stop the enemies from speeding by continously touching the freeze button
+3.Time reverse :-  Reverse the time and destroy the enemies that went always
+4.Hammer :- Call the Lord of Thunder for support and weaken the enemies
+5.Kill :- Burn all the enemies to death in a instant
+
+Idea:- I always played space game but I wished the player has some cool features rather than only bullets
+
+Future update: Add feature like boss and difficulty level
+
+Total commits :- 12 commits
+
+Days worked on :- 7 days
+
+Day 1:- created repository and made a plan for the project
+
+Day 2:- Started with background for the space game
+
+Day 3:- Added player for the space game
+
+Day 4:- Added Enemies who come randomly from different locations to destroy the player
+
+Day 5:- Gave bullets to player for destroying enemies
+
+Day 6:-  Added functionality like freeze, time, hammer and kill for the Player
+
+Day 7:- Fixed bugs and made this functionality working
